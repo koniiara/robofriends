3e02ca7cb2494a4df0f07c79a8d80db6954beb49
milepie <milena.piekoszewska@gmail.com>
2023-11-20 15:05:32 +0100

    adding new project

---
+++ src/containers/App.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import CardList from '../components/CardList.js';
+import SearchBox from '../components/SearchBox.js';
+import './App.css';
+import Scroll from '../components/Scroll.js'
+import ErrorBoundry from '../components/ErrorBoundry.js'
+
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			robots: [],
+			searchfield: ''
+		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users => this.setState({robots: users}));
+	}
+
+	onSearchChange = (event) => {
+		this.setState({searchfield: event.target.value})
+
+		
+		// console.log(filteredRobots);
+	}
+
+	render() {
+		const {robots, searchfield} = this.state;
+
+		const filteredRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+		})
+
+		// if (!robots.length) {
+		// 	return <h1>Loading</h1>
+		// } else {
+
+		// 	return (
+		// 	<div className ='tc'>
+		// 		<h1 className='f1'>RoboFriends</h1>
+
+		// 		<SearchBox searchChange={this.onSearchChange}/>
+		// 		<Scroll>
+		// 			<CardList robots = {filteredRobots}/>
+		// 		</Scroll>
+		// 	</div>
+		// 	);
+		// }
+		// to samo co poniżej
+
+		return !robots.length ?
+			<h1>Loading</h1> :
+		(
+			<div className ='tc'>
+				<h1 className='f1'>RoboFriends</h1>
+
+				<SearchBox searchChange={this.onSearchChange}/>
+				<Scroll>
+					<ErrorBoundry>
+						<CardList robots = {filteredRobots}/>
+					</ErrorBoundry>
+				</Scroll>
+			</div>
+		);
+	}
+}
+
+export default App;
